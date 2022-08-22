5d5b80f21cb74094fd8d95736f2a6b349a7f3dea
benben-student <a441832620@126.com>
2022-08-22 12:31:19 +0800

    feat: body page

--- src/Body/BodyPage.js
+++ src/Body/BodyPage.js
@@ -4,6 +4,10 @@ import imgB from "../Body/img/02.svg"
 import imgC from "../Body/img/03.svg"
 import imgD from "../Body/img/04.svg"
 import imgE from "../Body/img/05.svg"
+import imgF from "../Body/img/06.svg"
+import imgG from "../Body/img/07.svg"
+import imgH from "../Body/img/08.svg"
+import imgI from "../Body/img/09.svg"
 import './index.css'
 class BodyPage extends React.Component {
     render() {
@@ -26,7 +30,7 @@ class BodyPage extends React.Component {
 
                     <div>
                         <ul>
-                            <p>功能特性</p>
+                            <p className="bodypage-div-ul-p">功能特性</p>
                             <li>
                                 <img src={imgB} />
                                 <div><p>高性能</p></div>
@@ -53,8 +57,48 @@ class BodyPage extends React.Component {
                         </ul>
                     </div>
                 </div>
-                <div>3</div>
-                <div>4</div>
+                <div className="bodypage-div-ulB">
+                    <div className="bodypage-div-ulB-div">
+                        <div>开发资源</div>
+                        <ul>
+                            <li>
+                                <span className="bodypage-div-ul-li-spanA">在线体验</span><span className="bodypage-div-ul-li-spanB">开始体验</span>
+                                <p>在 Codesandbox 上在线体验，无需安装和配置环境</p>
+                                <img src={imgF} />
+                            </li>
+                            <li>
+                                <span className="bodypage-div-ul-li-spanA">组件列表</span><span className="bodypage-div-ul-li-spanB">查看全部</span>
+                                <p>基于蚂蚁集团的众多业务实践，抽象构建出移动端资产库</p>
+                                <img src={imgG} />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div >
+                    <div className="bodypage-div-ulC">
+                        <div className="bodypage-div-ulC-div">
+                            <div className="bodypage-div-ulC-div-div">新手指引</div>
+                            <ul>
+                                <li className="bodypage-div-ulC-ul">
+                                    <li>
+                                        <span >快速上手</span>
+                                        <p>轻松两步，快速实现安装和引入组件，Ant Design Mobile 自动加载 CSS 样式文件</p>
+                                        <div >查看详情</div>
+                                    </li>
+                                    <img src={imgH} />
+                                </li>
+                                <li className="bodypage-div-ulC-ul">
+                                    <li>
+                                        <span >常见问题</span>
+                                        <p>我们为你整理了一些常见的问题，遇到疑问不妨先来查阅一下</p>
+                                        <div >FAQ</div>
+                                    </li>
+                                    <img src={imgI} />
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
                 <div>5</div>
                 <div>6</div>
             </div>
