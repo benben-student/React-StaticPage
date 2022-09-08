d141ccb4124a56b84348ac6ed56b2b54710bf3d8
benben-student <a441832620@126.com>
2022-09-08 13:56:20 +0800

    feat : button components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Button";
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <Button/>
+            </div>
+        )
+    }
+}
+export default App
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+class Button extends React.Component {
+    render() {
+        return (
+            <>
+                <button>组件列表</button>
+            </>
+        )
+    }
+}
+export default Button
